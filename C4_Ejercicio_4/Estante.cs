15d20060902614deea8a4638437056ce831b6825
AlejoMCarmona <alejocarmona99@hotmail.com>
2022-09-17 19:01:07 -0300

    Ejercicio C4_Ejercicio_04 hecho

---
+++ C4_Ejercicio_4/Estante.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C4_Entidades_4
+{
+    public class Estante
+    {
+        private Producto[] productos;
+        private int ubicacionEstante;
+
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad]; 
+        }
+
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+
+        public Producto[] Productos { get { return this.productos; } }
+
+        public static string MostrarEstante(Estante e)
+        {
+            StringBuilder informacionEstante = new StringBuilder();
+
+            if (e is not null)
+            {
+                informacionEstante.AppendLine("UBICACIÓN DEL ESTANTE: " + e.ubicacionEstante);
+                informacionEstante.AppendLine("*********Productos*********");
+
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if (e.productos[i] is not null)
+                    {
+                        informacionEstante.AppendLine(Producto.MostrarProducto(e.productos[i]));
+                    }
+                }
+            } else
+            {
+                informacionEstante.AppendLine("");
+            }
+
+            return informacionEstante.ToString();
+        }
+
+        public static bool operator ==(Estante e, Producto p) 
+        {
+            bool retorno = false;
+
+            if (e is not null && p is not null)
+            {
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if (e.productos[i] == p)
+                    {
+                        retorno = true;
+                        break;
+                    }
+                }
+            }
+
+            return retorno;
+        }
+        public static bool operator !=(Estante e, Producto p) 
+        {
+            return !(e == p);
+        }
+
+        public static bool operator +(Estante e, Producto p)
+        {
+            bool retorno = false;
+
+            if (e is not null && p is not null && e != p)
+            {
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if (e.productos[i] is null)
+                    {
+                        e.productos[i] = p;
+                        retorno = true;
+                        break;
+                    }
+                }
+            }
+
+            return retorno;
+        }
+
+        public static bool operator -(Estante e, Producto p)
+        {
+            bool retorno = false;
+
+            if (e is not null && p is not null && e == p)
+            {
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if (e.productos[i] == p)
+                    {
+                        e.productos[i] = null;
+                        retorno = true;
+                        break;
+                    }
+                }
+            }
+
+            return retorno;
+        }
+    }
+}
---
+++ C4_Ejercicio_4/Producto.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+
+namespace C4_Entidades_4
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public float Precio { get { return this.precio; } }
+        public string Marca { get { return this.marca; } }
+
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.marca = marca;
+            this.codigoDeBarra = codigo;
+            this.precio = precio;
+        }
+
+        public static string MostrarProducto(Producto p)
+        {
+            StringBuilder producto = new StringBuilder();
+
+            if (p is not null)
+            {
+                producto.AppendLine("Marca: " + p.marca);
+                producto.AppendLine("Precio: " + p.precio);
+                producto.AppendLine("Código de barras: " + p.codigoDeBarra);
+            } else
+            {
+                producto.Append("");
+            }
+
+            return producto.ToString();
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+
+        public static bool operator ==(Producto p, string marca)
+        {
+            bool retorno = false;
+
+            if (p is not null && String.Compare(p.marca, marca) == 0)
+            {
+                retorno = true;
+            }
+
+            return retorno;
+        }
+
+        public static bool operator !=(Producto p, string marca) 
+        {
+            return !(p == marca);
+        }
+
+
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            bool retorno = false;
+
+            if (p1 is not null && p2 is not null && String.Compare(p1.marca, p2.marca) == 0 && String.Compare(p1.codigoDeBarra, p2.codigoDeBarra) == 0)
+            {
+                retorno = true;
+            }
+
+            return retorno;
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+    }
+}
