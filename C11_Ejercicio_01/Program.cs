7a1cbdd434e99deed3ada1842a5e5619b07cca89
AlejoMCarmona <alejocarmona99@hotmail.com>
2022-10-15 16:34:52 -0300

    'C11_Ejercicio_01' primera parte

---
+++ C11_Ejercicio_01/Program.cs
@@ -0,0 +1,36 @@
+﻿using C11_Entidades_01;
+using System;
+
+namespace C11_Ejercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultado;
+
+            try
+            {
+                resultado = Calculadora.Add("1");
+                Console.WriteLine($"Resultado: {resultado}");
+
+                resultado = Calculadora.Add("");
+                Console.WriteLine($"Resultado: {resultado}");
+
+                resultado = Calculadora.Add("1,2");
+                Console.WriteLine($"Resultado: {resultado}");
+
+                resultado = Calculadora.Add("1, hola"); //excepción
+                Console.WriteLine($"Resultado: {resultado}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ C11_Entidades_01/Calculadora.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace C11_Entidades_01
+{
+    public static class Calculadora
+    {
+        //Primera parte:
+        public static int Add (string numeros)
+        {
+            int resultado = 0;
+            string posibleNumero = "";
+            int contadorNumeros = 0;
+
+            if (!String.IsNullOrEmpty(numeros))
+            {
+                for (int i = 0; i < numeros.Length; i++)
+                {
+                    if (!numeros[i].Equals(',')) posibleNumero += numeros[i];
+                    if (contadorNumeros == 2) break;
+
+                    if ((numeros[i].Equals(',') || i == numeros.Length - 1))
+                    {
+                        try
+                        {
+                            resultado += int.Parse(posibleNumero);
+                            contadorNumeros++;
+                            posibleNumero = "";
+                        }
+                        catch (FormatException ex)
+                        {
+                            throw new FormatException(ex.Message);
+                        }
+                        catch (Exception ex)
+                        {
+                            throw new Exception(ex.Message);
+                        }
+                    }
+                }
+            }
+
+            return resultado;
+        }
+    }
+}
