b55be8bdec01cac8aaf08f73e60e92b1295692eb
AlejoMCarmona <alejocarmona99@hotmail.com>
2022-10-15 20:09:29 -0300

    Ejercicio 'C11_Ejercicio_03' hecho

--- C11_Ejercicio_01/Program.cs
+++ C11_Ejercicio_01/Program.cs
@@ -7,72 +7,72 @@ namespace C11_Ejercicio_01
     {
         static void Main(string[] args)
         {
-            int resultado;
+            //    int resultado;
 
-            try
-            {
-                resultado = Calculadora.Add("1");
-                Console.WriteLine($"Resultado: {resultado}");
+            //    try
+            //    {
+            //        resultado = Calculadora.Add("1");
+            //        Console.WriteLine($"Resultado: {resultado}");
 
-                resultado = Calculadora.Add("");
-                Console.WriteLine($"Resultado: {resultado}");
+            //        resultado = Calculadora.Add("");
+            //        Console.WriteLine($"Resultado: {resultado}");
 
-                resultado = Calculadora.Add("1,2");
-                Console.WriteLine($"Resultado: {resultado}");
+            //        resultado = Calculadora.Add("1,2");
+            //        Console.WriteLine($"Resultado: {resultado}");
 
-                resultado = Calculadora.Add("1,2,3,4");
-                Console.WriteLine($"Resultado: {resultado}");
+            //        resultado = Calculadora.Add("1,2,3,4");
+            //        Console.WriteLine($"Resultado: {resultado}");
 
-                resultado = Calculadora.Add("1,2,3");
-                Console.WriteLine($"Resultado: {resultado}");
+            //        resultado = Calculadora.Add("1,2,3");
+            //        Console.WriteLine($"Resultado: {resultado}");
 
-                resultado = Calculadora.Add("1,023,3");
-                Console.WriteLine($"Resultado: {resultado}");
+            //        resultado = Calculadora.Add("1,023,3");
+            //        Console.WriteLine($"Resultado: {resultado}");
 
-                resultado = Calculadora.Add("1,023,31,29,4,7,9,4,2,5");
-                Console.WriteLine($"Resultado: {resultado}");
+            //        resultado = Calculadora.Add("1,023,31,29,4,7,9,4,2,5");
+            //        Console.WriteLine($"Resultado: {resultado}");
 
-                resultado = Calculadora.Add("1,6,");
-                Console.WriteLine($"Resultado: {resultado}");
+            //        resultado = Calculadora.Add("1,6,");
+            //        Console.WriteLine($"Resultado: {resultado}");
 
-                resultado = Calculadora.Add("1,6, "); //excepción en parte dos
-                Console.WriteLine($"Resultado: {resultado}");
+            //        resultado = Calculadora.Add("1,6, "); //excepción en parte dos
+            //        Console.WriteLine($"Resultado: {resultado}");
 
-                resultado = Calculadora.Add("  1,  6,   "); //excepción en parte dos
-                Console.WriteLine($"Resultado: {resultado}");
+            //        resultado = Calculadora.Add("  1,  6,   "); //excepción en parte dos
+            //        Console.WriteLine($"Resultado: {resultado}");
 
-                resultado = Calculadora.Add("  1,  6,   "); //excepción en parte dos
-                Console.WriteLine($"Resultado: {resultado}");
+            //        resultado = Calculadora.Add("  1,  6,   "); //excepción en parte dos
+            //        Console.WriteLine($"Resultado: {resultado}");
 
-                resultado = Calculadora.Add("1 \n2,3");
-                Console.WriteLine($"Resultado: {resultado}");
+            //        resultado = Calculadora.Add("1 \n2,3");
+            //        Console.WriteLine($"Resultado: {resultado}");
 
-                resultado = Calculadora.Add("1 \n2"); ////; \ n1; 2
-                Console.WriteLine($"Resultado: {resultado}");
+            //        resultado = Calculadora.Add("1 \n2"); ////; \ n1; 2
+            //        Console.WriteLine($"Resultado: {resultado}");
 
-                resultado = Calculadora.Add("//; \n1; 2");
-                Console.WriteLine($"Resultado: {resultado}");
+            //        resultado = Calculadora.Add("//; \n1; 2");
+            //        Console.WriteLine($"Resultado: {resultado}");
 
-                resultado = Calculadora.Add("//a \n1a 2a9");
-                Console.WriteLine($"Resultado: {resultado}");
+            //        resultado = Calculadora.Add("//a \n1a 2a9");
+            //        Console.WriteLine($"Resultado: {resultado}");
 
-                resultado = Calculadora.Add("//a \n1a 2a9");
-                Console.WriteLine($"Resultado: {resultado}");
+            //        resultado = Calculadora.Add("//a \n1a 2a9");
+            //        Console.WriteLine($"Resultado: {resultado}");
 
-                resultado = Calculadora.Add("1,,2"); //excepcion
-                Console.WriteLine($"Resultado: {resultado}");
+            //        resultado = Calculadora.Add("1,,2"); //excepcion
+            //        Console.WriteLine($"Resultado: {resultado}");
 
-                resultado = Calculadora.Add("1, \n"); //excepcion
-                Console.WriteLine($"Resultado: {resultado}");
-            }
-            catch (FormatException ex)
-            {
-                Console.WriteLine(ex.Message);
-            } 
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            //        resultado = Calculadora.Add("1, \n"); //excepcion
+            //        Console.WriteLine($"Resultado: {resultado}");
+            //    }
+            //    catch (FormatException ex)
+            //    {
+            //        Console.WriteLine(ex.Message);
+            //    } 
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine(ex.Message);
+            //    }
         }
     }
 }
