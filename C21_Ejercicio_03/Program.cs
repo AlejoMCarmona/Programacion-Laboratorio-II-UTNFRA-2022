e5c931714b42d7196ec1084311e5110fcc095b6b
AlejoMCarmona <alejocarmona99@hotmail.com>
2022-10-22 15:48:19 -0300

    Ejercicio 'C21_Ejercicio_03' hecho

---
+++ C21_Ejercicio_03/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace C21_Ejercicio_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensaje = "Hola, cómo estás? Yo muy bien, ansioso por empezar a programar; aunque tengo ganas de mirar una serie también. " +
+                            "Qué tengas buena semana.";
+
+            Console.WriteLine(mensaje);
+            Console.WriteLine($"Cantidad de signos de puntuacion: {mensaje.ObtenerCantidadSignosPuntuacion()}");
+
+            mensaje = "Hola";
+            Console.WriteLine(mensaje);
+            Console.WriteLine($"Cantidad de signos de puntuacion: {mensaje.ObtenerCantidadSignosPuntuacion()}");
+        }
+    }
+}
---
+++ C21_Ejercicio_03/StringExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C21_Ejercicio_03
+{
+    public static class StringExtension
+    {
+        public static int ObtenerCantidadSignosPuntuacion(this string cadena)
+        {
+            int cantidad = 0;
+
+            foreach (char caracter in cadena)
+            {
+                if (caracter.CompareTo(',') == 0 || caracter.CompareTo('.') == 0 || caracter.CompareTo(';') == 0)
+                {
+                    cantidad++;
+                }
+            }
+
+            return cantidad;
+        }
+    }
+}
