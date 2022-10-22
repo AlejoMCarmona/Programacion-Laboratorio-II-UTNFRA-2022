9d4a13c4134cbced5bd55813ea2f845484b94e7c
AlejoMCarmona <alejocarmona99@hotmail.com>
2022-10-22 03:39:34 -0300

    'Ejercicio C13_Ejercicio_03' hecho

---
+++ C13_EntidadesCentralita_03/IGuardar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C13_EntidadesCentralita_03
+{
+    public interface IGuardar<T>
+        where T : class
+    {
+        string RutaDeArchivo { get; set; }
+
+        bool Guardar();
+        T Leer();
+    }
+}
---
+++ C13_EntidadesCentralita_03/Llamada.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Text;
+
+namespace C13_EntidadesCentralita_03
+{
+    public abstract class Llamada
+    {
+        #region Atributos
+        protected float duracion;
+        protected string nroDestino;
+        protected string nroOrigen;
+        #endregion
+
+        #region Constructores
+        public Llamada(float duracion, string nroDestino, string nroOrigen)
+        {
+            this.duracion = duracion;
+            this.nroDestino = nroDestino;
+            this.nroOrigen = nroOrigen;
+        }
+        #endregion
+
+        #region Propiedades
+        public float Duracion
+        {
+            get { return this.duracion; }
+        }
+
+        public string NroDestino
+        {
+            get { return this.nroDestino; }
+        }
+
+        public string NroOrigen
+        {
+            get { return this.nroOrigen; }
+        }
+
+        public abstract float CostoLlamada
+        {
+            get;
+        }
+        #endregion
+
+        #region Métodos/Sobrecargas
+        public static int OrdenarPorDuracion(Llamada llamada1, Llamada llamada2)
+        {
+            int retorno = 0;
+
+            if (llamada1.duracion > llamada2.duracion)
+            {
+                retorno = 1;
+            } else if (llamada1.duracion < llamada2.duracion)
+            {
+                retorno = -1;
+            }
+
+            return retorno;
+        }
+
+        protected virtual string Mostrar()
+        {
+            StringBuilder datosLlamada = new StringBuilder();
+
+            datosLlamada.AppendLine("Número de origen: " + this.nroOrigen);
+            datosLlamada.AppendLine("Número de destino: " + this.nroDestino);
+            datosLlamada.AppendLine("Duración: " + this.duracion);
+
+            return datosLlamada.ToString();
+        }
+
+        public static bool operator ==(Llamada l1, Llamada l2)
+        {
+            bool retorno = false;
+
+            if (l1.Equals(l2) && l1.nroDestino == l2.nroDestino && l1.nroOrigen == l2.nroOrigen)
+            {
+                retorno = true;
+            }
+
+            return retorno;
+        }
+
+        public static bool operator !=(Llamada l1, Llamada l2)
+        {
+            return !(l1 == l2);
+        }
+        #endregion
+    }
+}
